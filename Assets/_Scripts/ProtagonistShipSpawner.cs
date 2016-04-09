34788db095518ec72c11d373716228f6fbd1af3f
JoshSauter <sauterj@umich.edu>
2016-04-09 00:56:25 -0400

    Reworked how background ships are spawned

--- Assets/_Scripts/ProtagonistShipSpawner.cs
+++ Assets/_Scripts/ProtagonistShipSpawner.cs
@@ -8,9 +8,22 @@ public class ProtagonistShipSpawner : MonoBehaviour {
 	float angleOffsetMax = 3f;          //How far off-center of the screen the ship can aim towards
 	float minSpeed = 20f;				//Lower limit on new ship speed
 	float maxSpeed = 25f;               //Upper limit on new ship speed
-	float sizeVariance = 0.85f;         //Random variance in new ship sizes
 
-	float interactableShipThreshold = 0.2f;		//How close to sizeScale == 1 a ship has to be to be considered "in" the battlegrounds
+	float backgroundShipChance = 0.9f;			//Percent chance for a ship to spawn in the background
+	float minBackgroundShipSize = 0.15f;		//Lower limit on size of background ships
+	float maxBackgroundShipSize = 0.75f;         //Upper limit on size of background ships
+
+	float battlegroundsShipChance = 0.065f;      //Percent chance for a ship to spawn in the battlegrounds
+	float minBattlegroundShipSize = 0.95f;		//Lower limit on size of battleground ships
+	float maxBattlegroundShipSize = 1.05f;      //Upper limit on size of battleground ships
+
+	float foregroundShipChance = 0.05f;         //Percent chance for a ship to spawn in the foreground
+	float minForegroundShipSize = 2.5f;			//Lower limit on size of foreground ships
+	float maxForegroundShipSize = 5f;			//Upper limit on size of foreground ships
+
+	float interactableShipThreshold = 0.05f;	//How close to sizeScale == 1 a ship has to be to be considered "in" the battlegrounds
+	float backgroundShipColorDim = 0.75f;       //How much background and foreground ships' colors are dimmed
+
 
 	// Use this for initialization
 	IEnumerator Start () {
@@ -27,7 +40,7 @@ public class ProtagonistShipSpawner : MonoBehaviour {
 		int randIndex = Random.Range(0, protagonistShipPrefabs.Length);
 
 		//Determine the random size (and parallax) of the ship
-		float scaleFactor = Random.Range(1 - sizeVariance, 1 + sizeVariance);
+		float scaleFactor = GetScaleFactor();
 		bool backgroundShip = Mathf.Abs(1 - scaleFactor) > interactableShipThreshold;
 
 		//Give a random offset to the ship's position
@@ -55,7 +68,7 @@ public class ProtagonistShipSpawner : MonoBehaviour {
 		newShipSprite.sortingOrder = (int)((1 - scaleFactor) * -100);
 		//Dim the colors of background ships
 		if (backgroundShip) {
-			newShipSprite.color *= 0.75f;
+			newShipSprite.color *= backgroundShipColorDim;
 		}
 
 		//Aim the ship towards the center of the map (+/- angleOffset) if it's an interactable ship
@@ -72,5 +85,41 @@ public class ProtagonistShipSpawner : MonoBehaviour {
 		//Move the ship towards the center of the map at a random velocity in range
 		float randSpeed = Random.Range(minSpeed, maxSpeed);
 		newShipPhysics.velocity = newShip.transform.up * randSpeed * scaleFactor;
+
+		if (!backgroundShip) {
+			print("Look out, a ship has entered the battlegrounds!");
+		}
+	}
+
+	float GetScaleFactor() {
+		float[] weights = new float[]{foregroundShipChance, battlegroundsShipChance, backgroundShipChance};
+		float sumWeights = 0;
+		foreach (var weight in weights) {
+			sumWeights += weight;
+		}
+		float randVal = Random.Range(0, sumWeights);
+
+		//Determine where the ship will spawn
+		int i;
+		for (i = 0; i < weights.Length; i++) {
+			if (randVal < weights[i]) {
+				break;
+			}
+			randVal -= weights[i];
+		}
+
+		switch (i) {
+			//Spawn ship in foreground
+			case 0:
+				return Random.Range(minForegroundShipSize, maxForegroundShipSize);
+			//Spawn ship in battlegrounds
+			case 1:
+				return Random.Range(minBattlegroundShipSize, maxBattlegroundShipSize);
+			//Spawn ship in background
+			case 2:
+				return Random.Range(minBackgroundShipSize, maxBackgroundShipSize);
+			default:
+				throw new System.Exception("randVal: " + randVal + " > sumWeights: " + sumWeights);
+		}
 	}
 }
