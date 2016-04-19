ddc6861f69cce2c5c2e5938b3d44880fdac21217
jslawski <jslawski@umich.edu>
2016-04-19 00:08:53 -0400

    Reflector implemented

---
+++ Assets/_Scripts/Reflector.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using PolarCoordinates;
+
+public class Reflector : MonoBehaviour {
+
+	Material reflectorMat;
+	float reflectorDuration = 4f;
+	float reflectionVelocity = 10f;
+
+	public Player _owningPlayer = Player.none;
+	public Player owningPlayer {
+		get {
+			return _owningPlayer;
+		}
+		set {
+			_owningPlayer = value;
+			if (value != Player.none) {
+				reflectorMat.color = GameManager.S.players[(int)value].playerColor;			}
+		}
+	}
+
+	void Awake() {
+		reflectorMat = GetComponentInChildren<SpriteRenderer>().material;
+		StartCoroutine(DestroyReflector());
+	}
+
+	IEnumerator DestroyReflector() {
+		yield return new WaitForSeconds(reflectorDuration);
+		Destroy(gameObject);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		//Convert any bullet that enters the reflector, and shoot it back at the opponent
+		if (other.tag == "Bullet" && other.gameObject.GetComponent<Bullet>().owningPlayer != owningPlayer) {
+			float sprayRange = 3f;
+			
+			//Get the bullet that is getting reflected
+			Bullet otherBullet = other.gameObject.GetComponent<Bullet>();
+			Vector3 bulletPosition = otherBullet.gameObject.transform.position;
+
+			//Get the opponent player value of the bullet
+			Player otherPlayer = otherBullet.owningPlayer;
+			if (otherPlayer != Player.none) {
+				Vector3 otherPlayerPosition = GameManager.S.players[(int)otherPlayer].transform.position;
+
+				//Determine reflection vector
+				Vector3 sprayVector = new Vector3(0, Random.Range(-sprayRange, sprayRange), 0);
+				Vector3 reflectionVector = (otherPlayerPosition - bulletPosition + sprayVector).normalized;
+
+				//Reflect the bullet back at the opponent
+				otherBullet.owningPlayer = owningPlayer;
+				otherBullet.gameObject.GetComponent<PhysicsObj>().velocity = reflectionVector * reflectionVelocity;
+			}
+		}
+	}
+}
