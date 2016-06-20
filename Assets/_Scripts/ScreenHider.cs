677c0e775e55e98c0ab7760c85e1793f844cc3ed
jslawski <jslawski@umich.edu>
2016-06-20 18:45:24 -0400

    Hid bug with scene main showing loading process, updated music

---
+++ Assets/_Scripts/ScreenHider.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScreenHider : MonoBehaviour {
+	Image fadePanel;
+
+
+	// Use this for initialization
+	void Start () {
+		fadePanel = GameObject.Find("FadePanel").GetComponent<Image>();
+		Invoke("RevealScreen", 1f);
+	}
+	
+	void RevealScreen() {
+		Color curColor = fadePanel.color;
+		curColor.a = 0;
+		fadePanel.color = curColor;
+	}
+}
